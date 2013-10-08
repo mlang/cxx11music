f00c36fd93e959337b329e02f8af5cb4821ffc17
Mario Lang <mlang@delysid.org>
2013-10-08 20:39:59 +0200

    Forgot file.

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include "cxx11music.hpp"
+#define BOOST_TEST_MODULE cxx11musictest
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(append) {
+  using namespace musical;
+  stream s;
+  note n;
+  s.append(n);
+  BOOST_REQUIRE_EQUAL(n.offset(s), rational{});
+}
+
