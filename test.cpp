0ca862ebe9593d4a42c76f16e2617563163969d2
Mario Lang <mlang@delysid.org>
2013-10-08 22:51:44 +0200

    Make Boost.UTF libraries optional.

--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include "cxx11music.hpp"
 #define BOOST_TEST_MODULE cxx11musictest
+#ifdef BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#else
+#include <boost/test/included/unit_test.hpp>
+#endif
 
 BOOST_AUTO_TEST_CASE(append) {
   using namespace musical;
